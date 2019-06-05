f99e1e05b52e810838db11bf126383a7ff02f853
zhaoyanglin <zhaoyanglin@msn.com>
2019-06-04 21:38:55 -0500

    fixed port issue

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/calculator', calculatorRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
