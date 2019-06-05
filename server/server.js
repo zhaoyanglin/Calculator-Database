ae4be0a43c3d640863218dbbcad1f42450f9d550
zhaoyanglin <zhaoyanglin@msn.com>
2019-06-04 21:47:25 -0500

    fixed build issue

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/calculator', calculatorRoute);
 
+app.use(express.static('build'));
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
